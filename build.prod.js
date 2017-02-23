8dfb8e2b9d81f4b938320c53b6bd3ac6339ecb3c
Ian Storm Taylor <ian@ianstormtaylor.com>
2017-02-23 15:06:06 -0800

    Updates

--- build.prod.js
+++ build.prod.js
@@ -22327,66 +22327,57 @@ function _interopRequireDefault(obj) {
   return obj && obj.__esModule ? obj : { default: obj };
 }
 
-function scrollWrapperElements(element, options) {
-  var window = (0, _getWindow2.default)(element);
-  var elementRect = element.getBoundingClientRect();
-  var wrapper = element.parentNode;
-
-  while (wrapper != window.document.body) {
-    var wrapperRect = wrapper.getBoundingClientRect();
-    var margin = options.margin;
-    var deltaX = 0;
-    var deltaY = 0;
-
-    if (elementRect.top < wrapperRect.top + margin) {
-      deltaY = elementRect.top - wrapperRect.top - margin;
-    } else if (elementRect.bottom > wrapperRect.bottom - margin) {
-      deltaY = elementRect.bottom - wrapperRect.bottom + margin;
-    }
-    if (elementRect.left < wrapperRect.left + margin) {
-      deltaX = elementRect.left - wrapperRect.left - margin;
-    } else if (elementRect.right > wrapperRect.right - margin) {
-      deltaX = elementRect.right - wrapperRect.right + margin;
-    }
-    wrapper.scrollTop += deltaY;
-    wrapper.scrollLeft += deltaX;
-    wrapper = wrapper.parentNode;
-  }
-
-  return elementRect;
-}
-/**
- * Copied from `element-scroll-to`, but edited to account for `window` being
- * inside of iframes.
- *
- * https://github.com/webmodules/element-scroll-to
- */
-
-function scrollWindow(element, options, elementRect) {
-  var window = (0, _getWindow2.default)(element);
-  var margin = options.margin;
+function scrollWindow(window, cursorTop, cursorLeft, cursorHeight) {
   var deltaX = 0;
   var deltaY = 0;
+  var cursorBottom = cursorTop + cursorHeight;
 
-  if (elementRect.top < 0 + margin) {
-    deltaY = elementRect.top - margin;
-  } else if (elementRect.bottom > window.innerHeight - margin) {
-    deltaY = elementRect.bottom - window.innerHeight + margin;
+  if (cursorTop < 0 || cursorBottom > window.innerHeight) {
+    deltaY = cursorTop - window.scrollY + window.innerHeight / 2 - cursorHeight / 2;
   }
-  if (elementRect.left < 0 + margin) {
-    deltaX = elementRect.left - margin;
-  } else if (elementRect.right > window.innerWidth - margin) {
-    deltaX = elementRect.right - window.innerWidth + margin;
+
+  if (cursorLeft < 0 || cursorLeft > window.innerWidth) {
+    deltaX = cursorLeft - window.scrollX + window.innerWidth / 2;
   }
 
   window.scrollBy(deltaX, deltaY);
 }
+/**
+ * Helps scroll the cursor into the middle of view if it isn't in view
+ */
 
-function scrollTo(element, options) {
-  options = options || {};
-  options.margin = options.margin || 0;
-  var rect = scrollWrapperElements(element, options);
-  scrollWindow(element, options, rect);
+function scrollTo(element) {
+  var window = (0, _getWindow2.default)(element);
+  var s = window.getSelection();
+  if (s.rangeCount > 0) {
+    var selectionRect = s.getRangeAt(0).getBoundingClientRect();
+    var innerRect = selectionRect;
+    var wrapper = element;
+    var cursorHeight = innerRect.height;
+    var cursorTop = innerRect.top;
+    var cursorLeft = innerRect.left;
+
+    while (wrapper != window.document.body) {
+      var wrapperRect = wrapper.getBoundingClientRect();
+      var currentY = cursorTop;
+      var cursorBottom = cursorTop + cursorHeight;
+      if (cursorTop < wrapperRect.top || cursorBottom > wrapperRect.bottom) {
+        cursorTop = wrapperRect.top + wrapperRect.height / 2 - cursorHeight / 2;
+        wrapper.scrollTop += currentY - cursorTop;
+      }
+
+      var currentLeft = cursorLeft;
+      if (cursorLeft < wrapperRect.left || cursorLeft > wrapperRect.right) {
+        cursorLeft = wrapperRect.left + wrapperRect.width / 2;
+        wrapper.scrollLeft += currentLeft - cursorLeft;
+      }
+
+      innerRect = wrapperRect;
+      wrapper = wrapper.parentNode;
+    }
+
+    scrollWindow(window, cursorTop, cursorLeft, cursorHeight);
+  }
 }
 
 exports.default = scrollTo;
